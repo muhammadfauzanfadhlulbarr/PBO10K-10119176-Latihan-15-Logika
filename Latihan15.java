b5c89cbe6ede70a4f16b037a1945a06a53045bab
Muhammad Fauzan Fadhlulbarr <52595452+muhammadfauzanfadhlulbarr@users.noreply.github.com>
2020-11-04 15:12:40 +0700

    Update Latihan15.java

--- Latihan15.java
+++ Latihan15.java
@@ -1,3 +1,11 @@
+/**
+ * Latihan15
+ * Nama     : Muhammad Fauzan Fadhlulbarr
+ * Kelas    : PBO10K
+ * NIM      : 10119176
+ * Program  : Logika 15
+ */
+
 public class Latihan15 {
     public static void main(String[] args) {
         boolean a = true;
